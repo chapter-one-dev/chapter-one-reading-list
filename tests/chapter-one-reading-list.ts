60f67dbd12fac6890ff34d16e28fbeb1ee8c2c66
Jared Dunn <jdunn@webucator.com>
2022-07-13 18:44:15 -0400

    test: adding articles

--- tests/chapter-one-reading-list.ts
+++ tests/chapter-one-reading-list.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
+import { Program, Accounts, web3 } from "@project-serum/anchor";
 import { ChapterOneReadingList } from "../target/types/chapter_one_reading_list";
+import * as assert from "assert";
 
 describe("chapter-one-reading-list", () => {
   // Configure the client to use the local cluster.
@@ -8,9 +9,62 @@ describe("chapter-one-reading-list", () => {
 
   const program = anchor.workspace.ChapterOneReadingList as Program<ChapterOneReadingList>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("can add an article", async () => {
+    const article: web3.Keypair = web3.Keypair.generate();
+
+    const accounts: Accounts = {
+      article: article.publicKey,
+      collector: program.provider.publicKey,
+      systemProgram: web3.SystemProgram.programId,
+    }
+
+    await program.methods.addArticle('Article 1', 'https://article1.example.com')
+      .accounts(accounts)
+      .signers([article])
+      .rpc();
+
+    const articleAccount = await program.account.article.fetch(article.publicKey);
+
+    assert.equal(articleAccount.collector.toBase58(), program.provider.publicKey.toBase58());
+    assert.equal(articleAccount.title, 'Article 1');
+    assert.equal(articleAccount.link, 'https://article1.example.com');
+    assert.ok(articleAccount.timestamp);
+  });
+
+  it("can add an article from another user", async () => {
+    // set up otherUser account
+    const otherUser: web3.Keypair = anchor.web3.Keypair.generate();
+    // request money for other user
+    const signature: string = await program.provider.connection.requestAirdrop(otherUser.publicKey, 1000000000);
+
+    const latestBlockhash = await program.provider.connection.getLatestBlockhash();
+
+    const confirmationStrategy: web3.BlockheightBasedTransactionConfirmationStrategy = {
+      blockhash: latestBlockhash.blockhash,
+      lastValidBlockHeight: latestBlockhash.lastValidBlockHeight,
+      signature,
+    }
+    
+    await program.provider.connection.confirmTransaction(confirmationStrategy);
+    
+    const article: web3.Keypair = web3.Keypair.generate();
+
+    const accounts: Accounts = {
+      article: article.publicKey,
+      collector: otherUser.publicKey,
+      systemProgram: web3.SystemProgram.programId,
+    }
+
+    await program.methods.addArticle('Article 2', 'https://article2.example.com')
+      .accounts(accounts)
+      .signers([article, otherUser])
+      .rpc();
+
+    const articleAccount = await program.account.article.fetch(article.publicKey);
+
+    assert.equal(articleAccount.collector.toBase58(), otherUser.publicKey.toBase58());
+    assert.equal(articleAccount.title, 'Article 2');
+    assert.equal(articleAccount.link, 'https://article2.example.com');
+    assert.ok(articleAccount.timestamp);
   });
 });
