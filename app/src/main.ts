6af593efb59f7727431be5714824fa81238f08e8
Jared Dunn <jdunn@webucator.com>
2022-07-14 10:47:14 -0400

    chore: import wallet styles

--- app/src/main.ts
+++ app/src/main.ts
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import 'solana-wallets-vue/styles.css';
 import "./main.scss";
 
 createApp(App).use(store).use(router).mount("#app");
