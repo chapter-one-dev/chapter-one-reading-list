0552285f2f380bdabb447801b3ed455f54239ba7
Jared Dunn <jdunn@webucator.com>
2022-07-14 11:53:44 -0400

    feat: check wallet connected

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <div class="container-fluid">
     <h1>Home</h1>
     <WalletMultiButton />
+    <div v-if="workspace.wallet.value">Wallet connected!</div>
   </div>
 </template>
 
@@ -12,7 +13,11 @@ import {
   SolflareWalletAdapter,
 } from "@solana/wallet-adapter-wallets";
 
+import { initWorkspace, useWorkspace, Workspace } from "@/composables";
+
 const wallets = [new PhantomWalletAdapter(), new SolflareWalletAdapter()];
 
 initWallet({ wallets, autoConnect: true });
+initWorkspace();
+const workspace: Workspace = useWorkspace();
 </script>
