c722e6184d2b6632672ec64c798107a6c2eb32a7
Jared Dunn <jdunn@webucator.com>
2022-07-15 11:27:15 -0400

    feat: use vuex store to keep track of articles

--- app/src/store/index.ts
+++ app/src/store/index.ts
@@ -1,8 +1,34 @@
 import { createStore } from "vuex";
+import { fetchArticles } from "@/api";
+import { Article } from "@/models/Article";
+
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    articles: [] as Article[]
+  },
+  getters: {
+    getArticles(state) {
+      return state.articles;
+    }
+  },
+  mutations: {
+    addArticle(state, article: Article) {
+      state.articles.push(article);
+    },
+    setArticles(state, articles: Article[]) {
+      state.articles = articles;
+    },
+    deleteArticle(state, article: Article) {
+      const articleIndex = state.articles.indexOf(article);
+      state.articles.splice(articleIndex);
+    }
+  },
+  actions: {
+    async loadArticles({commit}) {
+      const articles: Article[] = await fetchArticles();
+      commit('setArticles', articles);
+    },
+  },
   modules: {},
 });
