7a5f53a865cc5f49c0439dc93ef72142aedcc2c3
Jared Dunn <jdunn@webucator.com>
2022-07-15 11:02:34 -0400

    content: about view

--- app/src/views/AboutView.vue
+++ app/src/views/AboutView.vue
@@ -1,5 +1,4 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <h1>About this site</h1>
+  <p>Jared Dunn, an intern at Chapter One, built this for Chapter One's first Moonshot week.</p>
 </template>
