04b38fd665f4ed9cbc76d41ce4b991b2953ae273
Jared Dunn <jdunn@webucator.com>
2022-07-15 11:01:00 -0400

    feat: add deletion to article card

--- app/src/components/ArticleCard.vue
+++ app/src/components/ArticleCard.vue
@@ -2,6 +2,7 @@
 import { Article } from "@/models/Article";
 import { defineComponent } from "vue";
 import LinkPreview from "./LinkPreview.vue";
+import { deleteArticle } from "@/api/delete-article";
 
 export default defineComponent({
   name: "ArticleCard",
@@ -13,16 +14,37 @@ export default defineComponent({
   },
   components: {
     LinkPreview,
+  },
+  methods: {
+    async deleteThisArticle() {
+      const deleted = await deleteArticle(this.article);
+      return deleted;
+    }
   }
 
 });
+</script>
+<script setup lang="ts">
+import { useWorkspace } from "@/composables";
+
+const {wallet} = useWorkspace();
 </script>
 
 <template>
   <div class="card">
     <LinkPreview :link="article.link" />
     <div class="card-body">
-      <a :href="article.link">Go to page</a>
+      <a :href="article.link" class="btn btn-secondary" target="_blank">Go to article!</a>
+      <p>Added {{article.created_ago}} by 
+        <a :href="`https://explorer.solana.com/address/${article.collector.toString()}?cluster=devnet`">
+        {{article.collector_display}}
+        </a>
+      </p>
+      <button v-if="wallet && wallet.publicKey && wallet.publicKey.toBase58() == article.collector.toBase58()"
+        @click="deleteThisArticle"
+        class="btn btn-danger">
+        Delete Article
+      </button>
     </div>
   </div>
 </template>
