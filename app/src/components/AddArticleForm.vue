82401a9e5ada42fc90406c5f8299f5960e4c4865
Jared Dunn <jdunn@webucator.com>
2022-07-14 22:28:58 -0400

    feat: add preview to article form

--- app/src/components/AddArticleForm.vue
+++ app/src/components/AddArticleForm.vue
@@ -1,30 +1,38 @@
 <template>
   <form>
     <div class="input-group flex-nowrap">
-      <label for="title" class="input-group-text">Article Title</label>
-      <input id="title" class="form-control" placeholder="MEV" v-model="title"/>
-    </div>
-    <div class="input-group flex-nowrap">
       <label for="link" class="input-group-text">Link</label>
       <input id="link" class="form-control" placeholder="https://twitter.com/yb_effect/status/1521609803600044032" v-model="link" />
     </div>
-    <button class="btn btn-secondary" @click.prevent="add">Add Article</button>
+    <button class="btn btn-primary" @click.prevent="add">Add Article</button>
+    <button v-if="!showPreview" class="btn btn-secondary" @click.prevent="togglePreview" :disabled="!validLink">Show Preview</button>
+    <button v-else class="btn btn-secondary" @click.prevent="togglePreview">Hide Preview</button>
   </form>
-  <!-- TODO: add preview article button-->
+  <aside v-if="showPreview">
+    <LinkPreview :link="link" />
+  </aside>
 </template>
 
 <script setup lang="ts">
-import {ref} from "vue";
+import {ref, computed} from "vue";
 import { addArticle } from '@/api/add-article';
+import LinkPreview from './LinkPreview.vue';
+import { isValidURL } from "@/composables";
 
-const title = ref("");
 const link = ref("");
+const showPreview = ref(false);
+const validLink = computed(() => {
+  return isValidURL(link.value);
+});
 
 async function add() {
-  const article = await addArticle(title.value, link.value);
-  title.value = "";
+  const article = await addArticle(link.value);
   link.value = "";
   console.log(article);
+}
+
+function togglePreview() {
+  showPreview.value = !showPreview.value;
 }
 
 </script>
