e86d031cb8d6d7e580d68bbd9f059e50e543fd09
Jared Dunn <jdunn@webucator.com>
2022-07-15 11:02:26 -0400

    style: home view

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,29 +1,18 @@
 <template>
-  <div class="container-fluid">
-    <h1>Home</h1>
-    <WalletMultiButton />
-    <div v-if="workspace.wallet.value">
-      <AddArticleForm />
-      <ArticleGrid />
-    </div>
-    
+  <div v-if="wallet">
+    <AddArticleForm />
+    <ArticleGrid />
+  </div>
+  <div v-else class="position-absolute top-50 start-50 translate-middle">
+    Connect your wallet to view and add to reading list!
   </div>
 </template>
 
 <script setup lang="ts">
-import { WalletMultiButton, initWallet } from "solana-wallets-vue";
-import {
-  PhantomWalletAdapter,
-  SolflareWalletAdapter,
-} from "@solana/wallet-adapter-wallets";
-
-import { initWorkspace, useWorkspace, Workspace } from "@/composables";
 import ArticleGrid from "@/components/ArticleGrid.vue";
 import AddArticleForm from "@/components/AddArticleForm.vue";
 
-const wallets = [new PhantomWalletAdapter(), new SolflareWalletAdapter()];
+import { useWorkspace } from "@/composables";
 
-initWallet({ wallets, autoConnect: true });
-initWorkspace();
-const workspace: Workspace = useWorkspace();
+const {wallet} = useWorkspace();
 </script>
